b6cc6ed5b855345936c249bc37b940eaef287be3
andikalp313 <andikaluhurpambudi@gmail.com>
2024-12-24 18:09:12 +0700

    fix: create event and create vocher

--- src/controller/event.controller.ts
+++ src/controller/event.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { createEventService } from "../services/events/create-event.service";
 import { getEventsService } from "../services/events/get.events.service";
 import { getEventService } from "../services/events/get.event.service";
+import { getEventsByUserService } from "../services/events/get-events-by-user.service";
 
 export const createEventController = async (
   req: Request,
@@ -54,6 +55,31 @@ export const getEventController = async (
   try {
     const id = Number(req.params.id);
     const result = await getEventService(id);
+    res.status(200).send(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getEventsByUserController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const userId = res.locals.user.id;
+
+    const result = await getEventsByUserService(userId);
+
+    if (result.length === 0) {
+      res.status(404).json({
+        status: "success",
+        message: "No events found.",
+      });
+
+      return;
+    }
+
     res.status(200).send(result);
   } catch (error) {
     next(error);
