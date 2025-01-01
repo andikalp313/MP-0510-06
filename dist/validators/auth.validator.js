262c518531d9e5e3f80099425f6c2e9d9bf429f7
inas9116 <139898118+inas9116@users.noreply.github.com>
2025-01-01 17:41:23 +0700

    update register admin (#17)
    oke

--- dist/validators/auth.validator.js
+++ dist/validators/auth.validator.js
@@ -1,10 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateResetPassword = exports.validateForgotPassword = exports.validateLogin = exports.validateRegister = void 0;
+exports.validateResetPassword = exports.validateForgotPassword = exports.validateLogin = exports.validateOrganizerRegister = exports.validateRegister = void 0;
 const express_validator_1 = require("express-validator");
 exports.validateRegister = [
     (0, express_validator_1.body)("name").notEmpty().withMessage("Name is required").isString(),
     (0, express_validator_1.body)("email").notEmpty().withMessage("Email is required").isEmail(),
+    (0, express_validator_1.body)("address").notEmpty().withMessage("Address is required"),
+    (0, express_validator_1.body)("password").notEmpty().withMessage("Password is required"),
+    (req, res, next) => {
+        const errors = (0, express_validator_1.validationResult)(req);
+        if (!errors.isEmpty()) {
+            res.status(400).send({ message: errors.array()[0].msg });
+            return;
+        }
+        next();
+    },
+];
+exports.validateOrganizerRegister = [
+    (0, express_validator_1.body)("name").notEmpty().withMessage("Name is required").isString(),
+    (0, express_validator_1.body)("organizerName").notEmpty().withMessage("name is requred").isString(),
+    (0, express_validator_1.body)("email").notEmpty().withMessage("Email is required").isEmail(),
+    (0, express_validator_1.body)("address").notEmpty().withMessage("address is required").isString(),
     (0, express_validator_1.body)("password").notEmpty().withMessage("Password is required"),
     (req, res, next) => {
         const errors = (0, express_validator_1.validationResult)(req);
--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -3,6 +3,7 @@ import { registerService } from "../services/auth/register.service";
 import { loginService } from "../services/auth/login.service";
 import { forgotPasswordService } from "../services/auth/forgot-password.service";
 import { resetPasswordService } from "../services/auth/reset-password.service";
+import { registerOrganizerService } from "../services/auth/register-organizer.service";
 
 export const registerController = async (
   req: Request,
@@ -16,6 +17,20 @@ export const registerController = async (
     next(error);
   }
 };
+
+export const registerAdminController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const result = await registerOrganizerService(req.body);
+    res.status(200).send(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const loginController = async (
   req: Request,
   res: Response,
