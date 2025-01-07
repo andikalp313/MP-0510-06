02d74d129b8004dc703949613af8d9d0fb85ec43
andikalp313 <andikaluhurpambudi@gmail.com>
2025-01-07 13:41:19 +0700

    netlify

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,4 +1,17 @@
 import ServerlessHttp from "serverless-http";
 import app from "../../src";
+import eventRouter from "../../src/routes/event.router";
+import authRouter from "../../src/routes/auth.router";
+import voucherRouter from "../../src/routes/voucher.router";
+import transactionRouter from "../../src/routes/transaction.router";
+import reviewRouter from "../../src/routes/review.router";
+import accountRouter from "../../src/routes/account.router";
+
+app.use("/events", eventRouter);
+app.use("/auth", authRouter);
+app.use("/profiles", accountRouter);
+app.use("/vouchers", voucherRouter);
+app.use("/transactions", transactionRouter);
+app.use("/reviews", reviewRouter);
 
 export const handler = ServerlessHttp(app);
